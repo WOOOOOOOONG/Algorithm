973ca897ab7ffbacc76e91ff383d180606815ced
WOOOOOOOONG <sterme9@naver.com>
2022-11-05 00:12:53 +0900

    Rename folder

---
+++ Project(IntelliJ)/Algorithm/src/Test/Nexon/NexSol1.java
@@ -0,0 +1,21 @@
+package Test.Nexon;
+
+public class NexSol1 {
+    public static void main(String[] args) {
+        System.out.println(minNum(3, 5, 5));
+    }
+    public static int minNum(int samDaily, int kellyDaily, int difference) {
+        int firstDiff = (samDaily+difference)-kellyDaily;
+        int dayDiff = kellyDaily-samDaily;
+
+        if (samDaily == kellyDaily && difference == 0) {
+            return -1;
+        }
+
+        int day = 1;
+
+        if (firstDiff >= 0) day += (firstDiff / dayDiff) + 1;
+
+        return day;
+    }
+}
